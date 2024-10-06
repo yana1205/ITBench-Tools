a2d021650532e526d7ddd2807523892ff02b9feb
TAKUMI YANAGAWA <YANA@jp.ibm.com>
2024-10-07 08:41:14 +0900

    reuse openllm leaderboard (#1)
    * update submodule
    
    Signed-off-by: Takumi Yanagawa <yana@jp.ibm.com>
    
    * submodules point main branch
    
    Signed-off-by: Takumi Yanagawa <yana@jp.ibm.com>
    
    * update submodule
    
    Signed-off-by: Takumi Yanagawa <yana@jp.ibm.com>
    
    * add unittest
    
    Signed-off-by: Takumi Yanagawa <yana@jp.ibm.com>
    
    * add unittest and refactor
    
    Signed-off-by: Takumi Yanagawa <yana@jp.ibm.com>
    
    * return jsonl for visualization
    
    Signed-off-by: Takumi Yanagawa <yana@jp.ibm.com>
    
    * openllm leaderboard
    
    Signed-off-by: Takumi Yanagawa <yana@jp.ibm.com>
    
    * update submodules
    
    Signed-off-by: Takumi Yanagawa <yana@jp.ibm.com>
    
    ---------
    
    Signed-off-by: Takumi Yanagawa <yana@jp.ibm.com>

---
+++ tests/bundle_statues.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from typing import List, Optional, Tuple
+
+from polaris_pac_bench.models.bundle import BundleCondition, BundleStatus
+
+
+def build(type_status_reason_trios: List[Tuple[str, str, Optional[str]]], current: Optional[BundleStatus] = None) -> BundleStatus:
+    kubeconfig = "TBD"
+    basetime = datetime(year=2024, month=10, day=1, hour=0, minute=0, second=0)
+    conditions = [BundleCondition(lastTransitionTime=basetime, type=x[0], status=x[1], reason=x[2]) for x in type_status_reason_trios]
+
+    if current:
+        new_conditions = []
+        for condition in current.conditions:
+            new_conditions.append(condition)
+
+        for condition in conditions:
+            founds = [x for x in enumerate(current.conditions) if x[1].type == condition.type]
+            if len(founds) > 0:
+                idx = founds[0][0]
+                new_conditions[idx] = condition
+    else:
+        new_conditions = conditions
+    return BundleStatus(kubeconfig=kubeconfig, conditions=new_conditions)
+
+
+INITIAL = build(
+    [
+        ("Deployed", "False", None),
+        ("FaultInjected", "False", None),
+        ("Destroyed", "False", None),
+    ]
+)
+DEPLOYING = build([], INITIAL)
+DEPLOYED = build(
+    [
+        ("Deployed", "True", None),
+    ],
+    DEPLOYING,
+)
+FAULT_INJECTING = build([], DEPLOYED)
+FAULT_INJECTED = build(
+    [
+        ("FaultInjected", "True", None),
+    ],
+    FAULT_INJECTING,
+)
+DESTROYING = build([], FAULT_INJECTED)
+DESTROYED = build(
+    [
+        ("Destroyed", "True", None),
+    ],
+    FAULT_INJECTED,
+)
---
+++ tests/test_benchmark.py
@@ -0,0 +1,89 @@
+import json
+import sys
+
+from polaris_pac_bench.agent_operator import AgentOperator
+from polaris_pac_bench.benchmark import benchmark, evaluate_agent
+from polaris_pac_bench.bundle_operator import BundleOperator
+from polaris_pac_bench.models.benchmark import BenchmarkResult
+from polaris_pac_bench.models.bundle import Bundle, BundleResult, BundleStatus
+from tests import bundle_statues
+
+
+def get_status() -> BundleStatus:
+    pass
+
+
+def get_evaluation():
+    pass
+
+
+def gen_mock_with_status_update(monkeypatch, bundle_statuses):
+    results = iter(bundle_statuses)
+    mock_get_bundle_status = lambda: next(results)
+    monkeypatch.setattr(sys.modules[__name__], "get_status", mock_get_bundle_status)
+
+
+def gen_mock_invoke_bundle(monkeypatch):
+
+    def mock_invoke_bundle(self, target, extra_args=[]):
+        if target == "get_status":
+            status_json = get_status().model_dump_json()
+            data = {"status": json.loads(status_json)}
+            return json.dumps(data)
+        elif target == "evaluate":
+            return json.dumps(get_evaluation())
+        elif target == "deploy_bundle":
+            gen_mock_with_status_update(monkeypatch, [bundle_statues.DEPLOYING, bundle_statues.DEPLOYED])
+        elif target == "inject_fault":
+            monkeypatch.setattr(sys.modules[__name__], "get_evaluation", lambda: {"pass": False, "report": []})
+            gen_mock_with_status_update(monkeypatch, [bundle_statues.FAULT_INJECTING, bundle_statues.FAULT_INJECTED, bundle_statues.FAULT_INJECTED])
+        elif target == "destroy_bundle":
+            gen_mock_with_status_update(monkeypatch, [bundle_statues.DESTROYING, bundle_statues.DESTROYED])
+
+    return mock_invoke_bundle
+
+
+def test_benchmark(monkeypatch):
+
+    agents = {
+        "agent1": True,
+        "agent2": False,
+    }
+
+    bundles = [Bundle(name="bundle", directory=".", status=bundle_statues.INITIAL)]
+    monkeypatch.setattr(BundleOperator, "invoke_bundle", gen_mock_invoke_bundle(monkeypatch))
+
+    def mock_invoke_agent(self, agent, bundle_operator: BundleOperator, kubeconfig, policy_reports):
+        monkeypatch.setattr(sys.modules[__name__], "get_evaluation", lambda: {"pass": agents[agent], "report": []})
+
+    monkeypatch.setattr(AgentOperator, "invoke_agent", mock_invoke_agent)
+
+    benchmark_results = benchmark("test", agents, bundles, False, False)
+
+    df = BenchmarkResult.to_dataframe(benchmark_results, exclude=[BenchmarkResult.Column.results])
+    pass
+
+
+def test_evaluate_agent(monkeypatch):
+
+    bundles = {
+        "bundle1": True,
+        "bundle2": False,
+    }
+
+    monkeypatch.setattr(BundleOperator, "invoke_bundle", gen_mock_invoke_bundle(monkeypatch))
+
+    def mock_invoke_agent(self, agent, bundle_operator: BundleOperator, kubeconfig, policy_reports):
+        monkeypatch.setattr(sys.modules[__name__], "get_evaluation", lambda: {"pass": bundles[bundle_operator.bundle.name], "report": []})
+
+    monkeypatch.setattr(AgentOperator, "invoke_agent", mock_invoke_agent)
+
+    bundle_results = evaluate_agent(
+        "agent1", [Bundle(name=x, directory=".", status=bundle_statues.INITIAL) for x in bundles.keys()], is_test=False, soft_delete=False
+    )
+    df = BundleResult.to_dataframe(bundle_results)
+    df[BundleResult.Column.ttr] = df[BundleResult.Column.ttr].dt.total_seconds()
+    assert not df[(df[BundleResult.Column.bundle] == "bundle1") & (df[BundleResult.Column.passed] == True)].empty
+    assert not df[
+        (df[BundleResult.Column.bundle] == "bundle2") & (df[BundleResult.Column.passed] == False) & (df[BundleResult.Column.ttr] > 10)
+    ].empty
