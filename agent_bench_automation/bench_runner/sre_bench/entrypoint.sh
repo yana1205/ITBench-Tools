d42ecca837be5be3a57cedcd36c1062bef8b4111
Pranjal Gupta <Pranjal.Gupta2@ibm.com>
2025-02-15 10:07:38 +0530

    SRE Bench Runner Helm Chart (#146)
    * Add helm chart for sre-bench-runner
    
    Signed-off-by: Pranjal-Gupta2 <pranjal.gupta2@ibm.com>
    
    * Add documentation for staging cluster
    
    Signed-off-by: Pranjal-Gupta2 <pranjal.gupta2@ibm.com>
    
    * Fix typos and update hyperlinks in helm README
    
    Signed-off-by: Pranjal-Gupta2 <pranjal.gupta2@ibm.com>
    
    ---------
    
    Signed-off-by: Pranjal-Gupta2 <pranjal.gupta2@ibm.com>

--- agent_bench_automation/bench_runner/sre_bench/entrypoint.sh
+++ agent_bench_automation/bench_runner/sre_bench/entrypoint.sh
@@ -3,6 +3,6 @@
 kops export kubecfg --admin --name=$AWX_CLUSTERNAME --state=s3://$S3_BUCKET_NAME --kubeconfig /tmp/$AWX_CLUSTERNAME.yaml
 cd /etc/agent-benchmark
 python agent_bench_automation/main.py runner \
-    -c ./docs/scenario-support/config-sre.yaml \
+    -c /tmp/config/config.yaml \
     --runner_id runner1 \
     --service_type SRE
