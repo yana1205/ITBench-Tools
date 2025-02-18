75b0db3f60ec934ec1bf3d37810d9502b4a99d6b
Pranjal Gupta <Pranjal.Gupta2@ibm.com>
2025-02-18 06:45:56 +0530

    Add parameter to set config path in bench runner (#147)
    Signed-off-by: Pranjal-Gupta2 <pranjal.gupta2@ibm.com>

--- agent_bench_automation/bench_runner/sre_bench/entrypoint.sh
+++ agent_bench_automation/bench_runner/sre_bench/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+PATH_TO_CONFIG=$1
+
 kops export kubecfg --admin --name=$AWX_CLUSTERNAME --state=s3://$S3_BUCKET_NAME --kubeconfig /tmp/$AWX_CLUSTERNAME.yaml
 cd /etc/agent-benchmark
 python agent_bench_automation/main.py runner \
-    -c /tmp/config/config.yaml \
+    -c $PATH_TO_CONFIG \
     --runner_id runner1 \
     --service_type SRE
